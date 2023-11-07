12003d269d2ee0dd03ce5571eccd790f3b8181fd
NotBruteForce <hariom.17127@knit.ac.in>
2023-11-07 21:43:29 +0530

    AdeptLaz

---
+++ bruteForce/AgeOfWar.java
@@ -0,0 +1,124 @@
+package org.example.bruteForce;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class AgeOfWar {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter your platoon (e.g., Spearmen#10;Militia#30;FootArcher#20):");
+            String myInput = sc.nextLine();
+            System.out.println("Please enter Opponent Platoon: ");
+            String opponentInput = sc.nextLine();
+            // call the function to get the winning sequence.
+            winningSequence(myInput, opponentInput);
+            System.out.print("Exit:Y/N  ");
+            String exitStatus = sc.nextLine().toUpperCase();
+            if (exitStatus.equals("Y")) break;
+        }
+        sc.close();
+    }
+
+    public static void winningSequence(String myInput, String opponentInput) {
+        Map<String, Integer> myPlatoons = parsePlatoons(myInput); //Store the Platoons and Strength in the Map.
+        Map<String, Integer> opponentPlatoons = parsePlatoons(opponentInput);//Store the Platoons and Strength in the Map.
+        List<String> myPlatoonOrder = new ArrayList<>(myPlatoons.keySet()); // store my platoon in the list
+// generating all the permutation of my platoon and check the winning possibility with opponent sequence
+        List<List<String>> permutations = generatePermutations(myPlatoonOrder);
+// iterate all the permutations
+        for (List<String> permutation : permutations) {
+// check the permutation for winning
+            int wins = simulateBattles(permutation, myPlatoons, opponentPlatoons);
+            if (wins >= 3) {
+                System.out.println(permutation.stream()
+                        .map(unit -> unit + "#" + myPlatoons.get(unit))
+                        .collect(Collectors.joining(";")));
+                return;
+            }
+        }
+
+        System.out.println("There is no chance of winning");
+    }
+
+    private static Map<String, Integer> parsePlatoons(String input) {
+        Map<String, Integer> platoons = new HashMap<>();
+        String[] parts = input.split(";");
+        for (String part : parts) {
+            String[] tokens = part.split("#");
+            try {
+                Integer.parseInt(tokens[1]);
+            } catch (NumberFormatException e) {
+                throw new NumberFormatException("Error: Invalid command syntax. Please provide numerical value for number of soldiers");
+            }
+            platoons.put(tokens[0], Integer.parseInt(tokens[1]));
+        }
+        return platoons;
+    }
+
+    private static List<List<String>> generatePermutations(List<String> platoonOrder) {
+        List<List<String>> permutations = new ArrayList<>();
+        permute(platoonOrder, 0, permutations);
+        return permutations;
+    }
+// recursive function to generate all the permutations
+    private static void permute(List<String> platoonOrder, int index, List<List<String>> permutations) {
+        if (index == platoonOrder.size()) {
+            permutations.add(new ArrayList<>(platoonOrder));
+        } else {
+            for (int i = index; i < platoonOrder.size(); i++) {
+                Collections.swap(platoonOrder, index, i);
+                permute(platoonOrder, index + 1, permutations);
+                Collections.swap(platoonOrder, index, i);
+            }
+        }
+    }
+
+    private static int simulateBattles(List<String> order, Map<String, Integer> myPlatoons, Map<String, Integer> opponentPlatoons) {
+        int wins = 0;
+        for (int i = 0; i < myPlatoons.size(); i++) {
+            String myUnit = order.get(i);
+            List<String> OpponentPlatoonOrder = new ArrayList<>(opponentPlatoons.keySet());
+
+            String opponentUnit = OpponentPlatoonOrder.get(i);
+            int outcome = calculateBattleOutcome(myUnit, myPlatoons.get(myUnit), opponentUnit, opponentPlatoons.get(opponentUnit));
+            if (outcome > 0) {
+                wins++;
+            }
+        }
+        return wins;
+    }
+// to check the battle outcome with current permutation sequence and opponent
+    private static int calculateBattleOutcome(String myUnit, Integer myCount, String opponentUnit, Integer opponentCount) {
+        if (myUnit.equals(opponentUnit)) {
+            return myCount.compareTo(opponentCount);
+        }
+        Integer effectiveOpponentCount =0;
+        Set<String> yourAdvantages = getAdvantages(myUnit);
+        Set<String> opponentAdvantages = getAdvantages(opponentUnit);
+        if (yourAdvantages.contains(opponentUnit)) {
+            effectiveOpponentCount = opponentCount / 2;
+        } else if(opponentAdvantages.contains(myUnit)){
+            effectiveOpponentCount = opponentCount * 2;
+        }
+        return (myCount >= effectiveOpponentCount) ? 1 : -1;
+    }
+// to store the one platoon's advantage over another
+    static Map<String, Set<String>> unitAdvantages = new HashMap<>();
+    static boolean hasBeenStored = false;
+
+    private static Set<String> getAdvantages(String unit) {
+        if(!hasBeenStored) {
+            unitAdvantages.put("Militia", new HashSet<>(Arrays.asList("Spearmen", "LightCavalry")));
+            unitAdvantages.put("Spearmen", new HashSet<>(Arrays.asList("LightCavalry", "HeavyCavalry")));
+            unitAdvantages.put("LightCavalry", new HashSet<>(Arrays.asList("FootArcher", "CavalryArcher")));
+            unitAdvantages.put("HeavyCavalry", new HashSet<>(Arrays.asList("Militia", "FootArcher", "LightCavalry")));
+            unitAdvantages.put("CavalryArcher", new HashSet<>(Arrays.asList("Spearmen", "HeavyCavalry")));
+            unitAdvantages.put("FootArcher", new HashSet<>(Arrays.asList("Militia", "CavalryArcher")));
+        }
+
+        return unitAdvantages.get(unit);
+    }
+}
+
---
+++ optimized/AgeOfWarOptimised.java
@@ -0,0 +1,140 @@
+package org.example.optimized;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class AgeOfWarOptimised {
+
+    // Lists to store the order of units for the user and the opponent
+    static List<String> myOrder = new ArrayList<>();
+    static List<String> opponentOrder = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            // Prompt the user to enter their platoon
+            System.out.println("Enter your platoon (e.g., Spearmen#10;Militia#30;FootArcher#20):");
+            String myInput = sc.nextLine();
+            System.out.println("Please enter Opponent Platoon: ");
+            String opponentInput = sc.nextLine();
+            // Call the function to determine the winning sequence
+            winningSequence(myInput, opponentInput);
+            System.out.print("Exit:Y/N  ");
+            String exitStatus = sc.nextLine().toUpperCase();
+            if (exitStatus.equals("Y")) break;
+            // Clear the lists for the next inputs.
+            myOrder.clear();
+            opponentOrder.clear();
+        }
+        sc.close();
+    }
+
+    public static void winningSequence(String myInput, String opponentInput) {
+        // Parse the platoons provided by the user and opponent
+        Map<String, Integer> myPlatoons = parsePlatoons(myInput, true);
+        Map<String, Integer> opponentPlatoons = parsePlatoons(opponentInput, false);
+        List<String> tempOpponentOrder = new ArrayList<>(opponentOrder);
+        List<String> canWinNothing = new ArrayList<>();
+
+        String[] bestSequence = new String[myOrder.size()];
+        int i = 0;
+        // Find the best platoons to fight
+        while (i < myPlatoons.size()) {
+            String bestPlatoon = findBestPlatoon(myOrder, opponentOrder, myPlatoons, opponentPlatoons, i++);
+            if (bestPlatoon == null) canWinNothing.add(myOrder.get(i - 1));
+            else {
+                int index = tempOpponentOrder.indexOf(bestPlatoon);
+                bestSequence[index] = myOrder.get(i - 1);
+                opponentOrder.remove(bestPlatoon);
+            }
+        }
+        int win = 0;
+        for (String s : bestSequence) {
+            if (s != null) win++;
+        }
+        if (win >= 3) {
+            int k = 0;
+            for (int j = 0; j < bestSequence.length; j++) {
+                if (bestSequence[j] == null) bestSequence[j] = canWinNothing.get(k++);
+            }
+            // Display the winning sequence
+            System.out.println("Winning Sequence");
+            System.out.println(Arrays.stream(bestSequence)
+                    .map(unit -> unit + "#" + myPlatoons.get(unit))
+                    .collect(Collectors.toList()));
+        } else {
+            System.out.println("There is no chance of winning");
+        }
+    }
+
+    private static Map<String, Integer> parsePlatoons(String order, boolean myorder) {
+        // Parse the platoon orders and store them in a map
+        Map<String, Integer> platoons = new HashMap<>();
+        String[] parts = order.split(";");
+        for (String part : parts) {
+            String[] tokens = part.split("#");
+            try {
+                Integer.parseInt(tokens[1]);
+            } catch (NumberFormatException e) {
+                throw new NumberFormatException("Error: Invalid command syntax. Please provide a numerical value for the number of soldiers");
+            }
+            if (myorder) myOrder.add(tokens[0]);
+            else opponentOrder.add(tokens[0]);
+            platoons.put(tokens[0], Integer.parseInt(tokens[1]));
+        }
+        return platoons;
+    }
+
+    private static String findBestPlatoon(List<String> myPlatoonOrder, List<String> opponentPlatoonOrder,
+                                          Map<String, Integer> myPlatoons, Map<String, Integer> opponentPlatoons, int index) {
+        // Find the best platoon to fight based on advantages
+        String bestPlatoonToFight = null;
+        int minAdvantage = Integer.MAX_VALUE; // Initialize to a large value
+
+        String myPlatoon = myPlatoonOrder.get(index);
+        int myCount = myPlatoons.get(myPlatoonOrder.get(index));
+        for (String opponentPlatoon : opponentPlatoonOrder) {
+            int opponentCount = opponentPlatoons.get(opponentPlatoon);
+            int advantage = calculateAdvantage(myPlatoon, myCount, opponentPlatoon, opponentCount);
+            if (advantage > 0 && advantage < minAdvantage) {
+                minAdvantage = advantage;
+                bestPlatoonToFight = opponentPlatoon;
+            }
+        }
+        return bestPlatoonToFight;
+    }
+
+    private static int calculateAdvantage(String myUnit, int myCount, String opponentUnit, int opponentCount) {
+        // Calculate the advantage of one unit over another
+        if (myUnit.equals(opponentUnit)) {
+            return myCount - opponentCount;
+        }
+
+        Set<String> myAdvantages = getAdvantages(myUnit);
+        Set<String> opponentAdvantages = getAdvantages(opponentUnit);
+
+        if (myAdvantages.contains(opponentUnit)) {
+            return myCount * 2 - opponentCount;
+        } else if (opponentAdvantages.contains(myUnit)) {
+            return myCount - opponentCount * 2;
+        }
+
+        return myCount - opponentCount;
+    }
+
+    static Map<String, Set<String>> unitAdvantages = new HashMap<>();
+    static boolean hasBeenStored = false;
+
+    private static Set<String> getAdvantages(String unit) {
+        // Store unit advantages in a map
+        if (!hasBeenStored) {
+            unitAdvantages.put("Militia", new HashSet<>(Arrays.asList("Spearmen", "LightCavalry")));
+            unitAdvantages.put("Spearmen", new HashSet<>(Arrays.asList("LightCavalry", "HeavyCavalry")));
+            unitAdvantages.put("LightCavalry", new HashSet<>(Arrays.asList("FootArcher", "CavalryArcher")));
+            unitAdvantages.put("HeavyCavalry", new HashSet<>(Arrays.asList("Militia", "FootArcher", "LightCavalry")));
+            unitAdvantages.put("CavalryArcher", new HashSet<>(Arrays.asList("Spearmen", "HeavyCavalry")));
+            unitAdvantages.put("FootArcher", new HashSet<>(Arrays.asList("Militia", "CavalryArcher")));
+        }
+        return unitAdvantages.get(unit);
+    }
+}
